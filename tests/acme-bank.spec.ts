f7ae8b9bdb4f1a017b13d5fc67857151b5377c0d
mjapplitools <matt.jasaitis@applitools.com>
2023-11-16 09:39:28 -0800

    simplifying code

--- tests/acme-bank.spec.ts
+++ tests/acme-bank.spec.ts
@@ -1,147 +1,59 @@
-// This test case spec contains everything needed to run a full visual test against the ACME bank site.
-// It runs the test once locally.
-// If you use the Ultrafast Grid, then it performs cross-browser testing against multiple unique browsers.
-
 import { test } from '@playwright/test';
-import {
-  BatchInfo,
-  Configuration,
-  EyesRunner,
-  ClassicRunner,
-  VisualGridRunner,
-  BrowserType,
-  DeviceName,
-  ScreenOrientation,
-  Eyes,
-  Target
-} from '@applitools/eyes-playwright';
+import { BatchInfo, Configuration, EyesRunner, VisualGridRunner, BrowserType, DeviceName, ScreenOrientation, Eyes, Target } from '@applitools/eyes-playwright';
 
-// Settings to control how tests are run.
-// These could be set by environment variables or other input mechanisms.
-// They are hard-coded here to keep the example project simple.
-export const USE_ULTRAFAST_GRID: boolean = true;
-
-// Applitools objects to share for all tests
 export let Batch: BatchInfo;
 export let Config: Configuration;
 export let Runner: EyesRunner;
 
-// This method sets up the configuration for running visual tests.
-// The configuration is shared by all tests in a test suite, so it belongs in a `beforeAll` method.
-// If you have more than one test class, then you should abstract this configuration to avoid duplication.
 test.beforeAll(async() => {
 
-  if (USE_ULTRAFAST_GRID) {
-    // Create the runner for the Ultrafast Grid.
-    // Concurrency refers to the number of visual checkpoints Applitools will perform in parallel.
-    // Warning: If you have a free account, then concurrency will be limited to 1.
+    // Tell Applitools SDK to run on the Ultrafast Grid
     Runner = new VisualGridRunner({ testConcurrency: 5 });
-  }
-  else {
-    // Create the classic runner.
-    Runner = new ClassicRunner();
-  }
+    Batch = new BatchInfo({name: `Applitools Quickstart`});
 
-  // Create a new batch for tests.
-  // A batch is the collection of visual checkpoints for a test suite.
-  // Batches are displayed in the Eyes Test Manager, so use meaningful names.
-  const runnerName = (USE_ULTRAFAST_GRID) ? 'Ultrafast Grid' : 'Classic runner';
-  Batch = new BatchInfo({name: `Example: Playwright TypeScript with the ${runnerName}`});
-
-  // Create a configuration for Applitools Eyes.
-  Config = new Configuration();
-
-  // Set the batch for the config.
-  Config.setBatch(Batch);
-
-  // If running tests on the Ultrafast Grid, configure browsers.
-  if (USE_ULTRAFAST_GRID) {
-
-    // Add 3 desktop browsers with different viewports for cross-browser testing in the Ultrafast Grid.
-    // Other browsers are also available, like Edge and IE.
-    Config.addBrowser(800, 600, BrowserType.CHROME);
-    Config.addBrowser(1600, 1200, BrowserType.FIREFOX);
-    Config.addBrowser(1024, 768, BrowserType.SAFARI);
-  
-    // Add 2 mobile emulation devices with different orientations for cross-browser testing in the Ultrafast Grid.
-    // Other mobile devices are available.
-    Config.addDeviceEmulation(DeviceName.iPhone_11, ScreenOrientation.PORTRAIT);
-    Config.addDeviceEmulation(DeviceName.Nexus_10, ScreenOrientation.LANDSCAPE);
-  }
+    Config = new Configuration();
+    Config.setBatch(Batch);
+    Config.addBrowsers(
+        { name: BrowserType.CHROME, width: 800, height: 1024 },
+        { name: BrowserType.FIREFOX, width: 1600, height: 1200 },
+        { name: BrowserType.SAFARI, width: 1024, height: 768 },
+        { chromeEmulationInfo: { deviceName: DeviceName.iPhone_11, screenOrientation: ScreenOrientation.PORTRAIT} },
+        { chromeEmulationInfo: { deviceName: DeviceName.Nexus_10, screenOrientation: ScreenOrientation.LANDSCAPE} }
+    )
 });
 
-// This "describe" method contains related test cases with per-test setup and cleanup.
-// In this example, there is only one test.
 test.describe('ACME Bank', () => {
+    let eyes: Eyes;
+    test.beforeEach(async ({ page }) => {
+        eyes = new Eyes(Runner, Config);
 
-  // Test-specific objects
-  let eyes: Eyes;
+        // Start Applitools Visual AI Test
+        // Args: Playwright Page, App Name, Test Name, Viewport Size for local driver
+        await eyes.open(page, 'ACME Bank', test.info().title, { width: 1200, height: 600 })
+    });
+    
+    test('log into a bank account', async ({ page }) => {
+        await page.goto('https://sandbox.applitools.com/bank?layoutAlgo=true');
 
-  // This method sets up each test with its own Applitools Eyes object.
-  test.beforeEach(async ({ page }) => {
+        // Full Page - Visual AI Assertion
+        await eyes.check('Login page', Target.window().fully());
 
-    // Create the Applitools Eyes object connected to the runner and set its configuration.
-    eyes = new Eyes(Runner, Config);
+        await page.locator('id=username').fill('user');
+        await page.locator('id=password').fill('password');
+        await page.locator('id=log-in').click();
 
-    // Open Eyes to start visual testing.
-    // Each test should open its own Eyes for its own snapshots.
-    // It is a recommended practice to set all four inputs below:
-    await eyes.open(
-      
-      // The Playwright page object to "watch"
-      page,
-      
-      // The name of the application under test.
-      // All tests for the same app should share the same app name.
-      // Set this name wisely: Applitools features rely on a shared app name across tests.
-      'ACME Bank',
-      
-      // The name of the test case for the given application.
-      // Additional unique characteristics of the test may also be specified as part of the test name,
-      // such as localization information ("Home Page - EN") or different user permissions ("Login by admin"). 
-      test.info().title,
-      
-      // The viewport size for the local browser.
-      // Eyes will resize the web browser to match the requested viewport size.
-      // This parameter is optional but encouraged in order to produce consistent results.
-      { width: 1200, height: 600 });
-  });
-  
-  // This test covers login for the Applitools demo site, which is a dummy banking app.
-  // The interactions use typical Playwright calls,
-  // but the verifications use one-line snapshot calls with Applitools Eyes.
-  // If the page ever changes, then Applitools will detect the changes and highlight them in the Eyes Test Manager.
-  // Traditional assertions that scrape the page for text values are not needed here.
-  test('log into a bank account', async ({ page }) => {
+        // Full Page - Visual AI Assertion
+        await eyes.check('Main page', Target.window().fully().layout());
+    });
 
-    // Load the login page.
-    await page.goto('https://demo.applitools.com');
-
-    // Verify the full login page loaded correctly.
-    await eyes.check('Login page', Target.window().fully());
-
-    // Perform login.
-    await page.locator('id=username').fill('andy');
-    await page.locator('id=password').fill('i<3pandas');
-    await page.locator('id=log-in').click();
-
-    // Verify the full main page loaded correctly.
-    // This snapshot uses LAYOUT match level to avoid differences in closing time text.
-    await eyes.check('Main page', Target.window().fully().layout());
-  });
-  
-  // This method performs cleanup after each test.
-  test.afterEach(async () => {
-
-    // Close Eyes to tell the server it should display the results.
-    await eyes.close();
-  });
+    test.afterEach(async () => {
+        // End Applitools Visual AI Test
+        await eyes.close();
+    });
 });
 
 test.afterAll(async() => {
-
-  // Close the batch and report visual differences to the console.
-  // Note that it forces Playwright to wait synchronously for all visual checkpoints to complete.
-  const results = await Runner.getAllTestResults();
-  console.log('Visual test results', results);
+    // Wait for Ultrast Grid Renders to finish and gather results
+    const results = await Runner.getAllTestResults();
+    console.log('Visual test results', results);
 });
