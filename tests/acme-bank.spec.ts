805b386314ef8d643cfcda74e5d0803f630b22bd
mjapplitools <matt.jasaitis@applitools.com>
2023-12-12 09:11:48 -0800

    updated test name

--- tests/acme-bank.spec.ts
+++ tests/acme-bank.spec.ts
@@ -29,7 +29,7 @@ test.describe('ACME Bank', () => {
 
         // Start Applitools Visual AI Test
         // Args: Playwright Page, App Name, Test Name, Viewport Size for local driver
-        await eyes.open(page, 'ACME Bank', `Playwright Typescript: ${test.info().title}`, { width: 1200, height: 600 })
+        await eyes.open(page, 'ACME Bank', `Playwright Typescript: Quickstart`, { width: 1200, height: 600 })
     });
     
     test('log into a bank account', async ({ page }) => {
